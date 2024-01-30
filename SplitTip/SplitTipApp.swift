50125a6b9d64da2831111328774afc29ab9d723a
ekayaint <ekayaint@gmail.com>
2024-01-30 21:02:35 +0300

    Initial Commit

---
+++ SplitTip/SplitTipApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitTipApp.swift
+//  SplitTip
+//
+//  Created by ekayaint on 30.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct SplitTipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
